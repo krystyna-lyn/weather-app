71f7c3b2b20ebdbf357785b14047ff1ab846d452
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-12-04 22:14:41 +0000

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+
+import SearchBar from './components/SearchBar'
+import CurrentWeather from './components/CurrentWeather'
+import HourlyForeCast from './components/HourlyForeCast'
+import WeeklyForecast from './components/WeeklyForecast'
+import { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
+import { fetchWeatherData as getWeatherData } from './api'
 
 function App() {
     const [city, setCity] = useState('');
@@ -10,7 +17,18 @@ function App() {
     useEffect(() => {
         const fetchWeather = async () => {
             setLoading(true);
+            setError('');
+
             try {
+                const data = await getWeatherData(city);
+                const [mintemp_c, maxtemp_c] = data.forecast.forecastday[0].day;
+
+                setWeatherData({
+                    current: { ...data.current, mintemp_c, maxtemp_c },
+                    hourly: data.forecast.forecastday[0].day,
+                    weekly: data.forecast.forecastday.slice(1),
+                    location: data.location
+                })
 
             } catch (err) {
                 setError('Failed to fetch weather data');
@@ -18,28 +36,31 @@ function App() {
                 setLoading(false);
             }
         }
+        fetchWeather();
     }, [city])
 
-}
 
+    return (
+        <div className="App">
+            <div className='container'>
+                <SearchBar onSearch={setCity}>
+                    {loading && <p>Loading...</p>}
+                    {error && <p className="error">{error}</p>}
+                    {weatherData && (
+                        <>
+                            <CurrentWeather data={weatherData.current}
+                                location={weatherData.location}
+                            />
 
-return (
-    <div className="App">
-        <div className='container'>
-            <SearchBar onSearch={setCity}>
-                {loading && <p>Loading...</p>}
-                {error && <p className="error">{error}</p>}
-                {weatherData && (
-                    <>
-                        <CurrentWeather />
-                        <HourlyForeCast />
-                        <WeeklyForecast />
-                    </>
-                )}
+                            <HourlyForeCast data={weatherData.hourly} />
+                            <WeeklyForecast data={weatherData.weekly} />
+                        </>
+                    )}
 
-            </SearchBar>
+                </SearchBar>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default App;
---
+++ frontend/src/components/HourlyForeCast.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HourlyForeCast = () => {
+    return (
+        <div>HourlyForeCast</div>
+    )
+}
+
+export default HourlyForeCast

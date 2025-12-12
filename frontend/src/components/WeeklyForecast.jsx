f3a3edd601c51b8fd13223d3b1faa4ef14000751
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2025-12-12 18:57:52 +0000

    update WeeklyForcast component

--- frontend/src/components/WeeklyForecast.jsx
+++ frontend/src/components/WeeklyForecast.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { parseISO, format } from 'date-fns';
 
-const WeeklyForecast = () => {
+const WeeklyForecast = ({ data }) => {
     return (
-        <div>WeeklyForecast</div>
+        <div className='weekly-forecast-container'>
+            <h2>Weekly Forecast</h2>
+            {data.map((day, index) => {
+                return (
+                    <div className='card-weekly' key='index'>
+                        <div className='day-name'>
+                            {format(parseISO(day.date), 'EEE')}
+                        </div>
+                        <img src={day.day.condition.icon} className='day-icon' alt='icon' />
+                        <span className='day-text'>{day.day.condition.text}</span>
+
+                        <div className='hour-temp'>
+                            {Math.round(day.day.maxtemp_c)}°C
+                            {Math.round(day.day.mintemp_c)}°C
+                        </div>
+                        <div className='day-rain'>💧 {day.day.daily_chance_of_rain}%
+
+                        </div>
+                    </div>
+                )
+            }
+            )}
+        </div>
     )
 }
 
